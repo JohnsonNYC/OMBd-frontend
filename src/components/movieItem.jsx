8446a09413e88a94655aad0d6aa3eee3dac9efaf
JohnsonNYC <jkow95@gmail.com>
2020-08-31 12:42:37 -0400

    disabled button once movie was nominated

--- src/components/movieItem.jsx
+++ src/components/movieItem.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MovieItem = (props) => {
     const { movie, nominate } = props
+    const [button, setButton] = useState(false)
     return (
         <div>
-            <span>{movie.Title}({movie.Year})</span> <button onClick={() => nominate(movie)}> Nominate </button>
+            <span>{movie.Title}({movie.Year})</span> <button disabled={button} onClick={() => { nominate(movie); setButton(!button) }}> Nominate </button>
         </div>
     );
 }
