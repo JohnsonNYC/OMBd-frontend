daa9afb03465f61f449d6594e34f929012cfacf5
JohnsonNYC <jkow95@gmail.com>
2020-09-03 14:42:49 -0400

    added animation when loading nominee

--- src/components/nomineeItem.jsx
+++ src/components/nomineeItem.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import { Button, Card, CardImg, CardBody, CardTitle, CardSubtitle, Col } from 'reactstrap'
+import { useSpring, animated } from 'react-spring'
+import { Button, Card, CardBody, CardTitle, CardSubtitle, Col } from 'reactstrap'
 
 const NomineeItem = (props) => {
     const { movie, remove } = props
     return (
-        <Col sm="6">
-            <Card>
-                <img width="100%" style={{height:'auto', maxHeight: `350px`,width:'auto',maxWidth:'250px'}} src={movie.Poster} alt={movie.Title} />
-                <CardBody body>
-                    <CardTitle>{movie.Title}</CardTitle>
-                    <CardSubtitle>{movie.Year}</CardSubtitle>
-                </CardBody>
-                <CardBody>
-                    <Button color='warning' onClick={() => remove(movie)}> Remove </Button>
-                </CardBody>
-            </Card>
+
+        <Col sm="6" >
+            <animated.div style={useSpring({opacity:1, from:{opacity:0}})} className='animated'>
+                <Card >
+                    <img width="100%" style={{ height: 'auto', maxHeight: `350px`, width: 'auto', maxWidth: '250px', alignSelf: 'center' }} src={movie.Poster} alt={movie.Title} />
+                    <CardBody >
+                        <CardTitle>{movie.Title}</CardTitle>
+                        <CardSubtitle>{movie.Year}</CardSubtitle>
+                    </CardBody>
+                    <CardBody>
+                        <Button color='warning' onClick={() => remove(movie)}> Remove </Button>
+                    </CardBody>
+                </Card>
+            </animated.div>
         </Col>
     );
 }
